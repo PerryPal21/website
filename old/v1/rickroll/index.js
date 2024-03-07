09b3c41e15e0d3516d03512c38a3f97a4f5b1876
PerryPal21 <parmeet2102@gmail.com>
2024-03-07 09:49:40 -0500

    removed rickroll

--- old/v1/rickroll/index.js
+++
@@ -1,5 +0,0 @@
-function start() {
-    document.getElementById("audio").play();
-    document.querySelector(".btn").style.display = "none"
-    document.querySelector(".hidden").style.display = "block"
-}
